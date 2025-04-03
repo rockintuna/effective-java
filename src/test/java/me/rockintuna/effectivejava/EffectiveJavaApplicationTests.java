a4b38747e646d9f4f0eec5ea9d5eb98de6866978
rockintuna <rockintuna@naver.com>
2025-04-03 22:36:16 +0900

    Initial commit

---
+++ src/test/java/me/rockintuna/effectivejava/EffectiveJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package me.rockintuna.effectivejava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EffectiveJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
